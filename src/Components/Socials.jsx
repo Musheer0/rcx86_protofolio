5bab3a816e2047a74a47d37a6b8493a6c8e64193
Musheer <musheeran165@gmail.com>
2024-07-05 18:23:03 +0530

    fix social urls

--- src/Components/Socials.jsx
+++ src/Components/Socials.jsx
@@ -38,10 +38,12 @@ const SocialComp= () => {
         <h3 className='text-xl text-center font-semibold'>Lets Chat</h3>
        <div className="socials flex flex-wrap gap-2 items-center justify-center w-full">
        {socials.map((e,i)=>{
-            return <div key={i} className='flex items-center justify-center  gap-1 px-2 py-1 rounded-md border-[1px] bg-zinc-100 '>
+            return <a href={e.url ? e.url: '#'}>
+              <div key={i} className='flex items-center justify-center  gap-1 px-2 py-1 rounded-md border-[1px] bg-zinc-100 '>
                <span> {e.icon}</span>
                 <p>{e.name}</p>
             </div>
+            </a>
         })}
        </div>
     </section>
